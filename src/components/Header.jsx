20a618cb4678bc2f076926ef5b76e457ef2684ee
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-07-12 22:35:49 +0300

    Added images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,54 @@
-import Image from 'next/image'
-import bg from '../../assets/bg.jpeg'
+'use client'
 import React from 'react'
+import Image from 'next/image'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+
+import bg1 from '../../assets/bg.jpeg'
+import bg2 from '../../assets/enlarge_solar.png'
 
 function Header() {
   return (
     <div id="home" className="relative flex w-full h-screen">
       {/* Image */}
       <Image
-        src={bg}
+        src={bg1}
         layout="fill"
         objectFit="cover"
         style={{
           zIndex: -1,
         }}
       ></Image>
+      {/* <Carousel
+        autoPlay
+        infiniteLoop
+        showThumbs={false}
+        showStatus={false}
+        interval={5000} // Interval between slides in milliseconds (5 seconds in this example)
+        transitionTime={500} // Transition duration in milliseconds
+      >
+        <div>
+          <Image
+            src={bg1}
+            objectFit="cover"
+            fill={true}
+            style={{
+              zIndex: -1,
+            }}
+          />
+        </div>
+        <div>
+          <Image
+            src={bg2}
+            fill={true}
+            objectFit="cover"
+            style={{
+              zIndex: -1,
+            }}
+          />
+        </div>
+      </Carousel> */}
+
       <div
         className="absolute inset-0 bg-black opacity-50"
         style={{
